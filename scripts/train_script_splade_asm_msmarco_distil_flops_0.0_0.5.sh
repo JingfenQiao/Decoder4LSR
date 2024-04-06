8faaece39c4ab5a99988977c72a5979a20a6be43
JingfenQiao <jingfenqiao@gmail.com>
2024-04-06 10:53:46 +0200

    tuning

--- scripts/train_script_splade_asm_msmarco_distil_flops_0.0_0.5.sh
+++ scripts/train_script_splade_asm_msmarco_distil_flops_0.0_0.5.sh
@@ -20,7 +20,7 @@ CUDA_VISIBLE_DEVICES=0  nohup python -m lsr.train +experiment=mlp_encoder_only_t
 
 CUDA_VISIBLE_DEVICES=0  nohup python -m lsr.train +experiment=mlp_decoder_only_t5_base training_arguments.fp16=True wandb.resume=False > log/mlp_decoder_only_t5_base.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=0  nohup python -m lsr.train +experiment=mlp_encoder_decoder_multi_t5_base training_arguments.fp16=True wandb.resume=False > log/mlp_encoder_decoder_multi_t5_base.log 2>&1 &
+CUDA_VISIBLE_DEVICES=0  nohup python -m lsr.train +experiment=mlp_encoder_decoder_multi_t5_base training_arguments.fp16=True wandb.resume=False > log/mlp_encoder_decoder_multi_t5_base.log2 2>&1 &
 
 CUDA_VISIBLE_DEVICES=0  nohup python -m lsr.train +experiment=mlp_encoder_decoder_single_t5_base training_arguments.fp16=True wandb.resume=False > log/mlp_encoder_decoder_single_t5_base.log 2>&1 &
 
