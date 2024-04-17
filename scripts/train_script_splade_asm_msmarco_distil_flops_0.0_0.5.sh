6a116210f4e8294a908c23bdb1417c788ba73912
JingfenQiao <jingfenqiao@gmail.com>
2024-04-17 23:17:58 +0200

    multi steps

--- scripts/train_script_splade_asm_msmarco_distil_flops_0.0_0.5.sh
+++ scripts/train_script_splade_asm_msmarco_distil_flops_0.0_0.5.sh
@@ -54,6 +54,9 @@ CUDA_VISIBLE_DEVICES=0,1 nohup python -m lsr.train +experiment=mlm_decoder_only_
 CUDA_VISIBLE_DEVICES=0,1 nohup python -m lsr.train +experiment=mlm_decoder_only_opt27_lora_0.001 training_arguments.fp16=True wandb.resume=False > log/mlm_decoder_only_opt27_0.001.log 2>&1 &
 
 
-## larger teacher 
+## smaller teacher 
+CUDA_VISIBLE_DEVICES=0,1,2  nohup python -m lsr.train +experiment=mlm_encoder_decoder_multi_t5_base_0.01 training_arguments.fp16=True wandb.resume=False > log/mlm_encoder_decoder_multi_t5_base_0.01.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=0,1,2  nohup python -m lsr.train +experiment=rankllama_mlm_encoder_decoder_multi_t5_base_0.0001 training_arguments.fp16=True wandb.resume=False > log/rankllama_mlm_encoder_decoder_multi_t5_base_0.0001.log 2>&1 &
+
+## larger teacher 
+CUDA_VISIBLE_DEVICES=0,1,2  nohup python -m lsr.train +experiment=rankllama_mlm_encoder_decoder_multi_t5_base_0.01 training_arguments.fp16=True wandb.resume=False > log/rankllama_mlm_encoder_decoder_multi_t5_base_0.0001.log 2>&1 &
